bd5850d5b195a40696c514d74ff58cda033a645b
HBilel101 <hbilel992@gmail.com>
2024-12-09 15:58:55 +0100

    changed the base_URL

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,19 +4,21 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 // Register a new user
 export const registerUser = async (userData) => {
-    const response = await axios.post(`${API_URL}/auth/register`, userData);
+    console.log(API_URL)
+    const response = await axios.post("http://localhost:5000/api/auth/register", userData);
     return response.data;
 };
 
 // Log in a user
 export const loginUser = async (credentials) => {
-    const response = await axios.post(`${API_URL}/auth/login`, credentials);
+    console.log(API_URL)
+    const response = await axios.post("http://localhost:5000/api/auth/login", credentials);
     return response.data;
 };
 
 // Get the current user's profile (protected route)
 export const getProfile = async (token) => {
-    const response = await axios.get(`${API_URL}/auth/profile`, {
+    const response = await axios.get("http://localhost:5000/api/auth/profile", {
         headers: { Authorization: `Bearer ${token}` },
     });
     return response.data;
--- src/api/company.js
+++ src/api/company.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "http://localhost:5000/api/companies";
 
 // Helper to get authorization headers
 const getAuthHeader = () => {
@@ -10,7 +10,7 @@ const getAuthHeader = () => {
 
 // Create a company
 export const createCompany = async (data) => {
-    const response = await axios.post(`${API_URL}/company`, data, {
+    const response = await axios.post(`${API_URL}/`, data, {
         headers: getAuthHeader(),
     });
     return response.data;
@@ -18,7 +18,7 @@ export const createCompany = async (data) => {
 
 // Get the list of companies for the user
 export const getUserCompanies = async () => {
-    const response = await axios.get(`${API_URL}/company/list`, {
+    const response = await axios.get(`${API_URL}/list`, {
         headers: getAuthHeader(),
     });
     return response.data;
@@ -26,7 +26,7 @@ export const getUserCompanies = async () => {
 
 // Update a company (except status)
 export const updateCompany = async (id, data) => {
-    const response = await axios.put(`${API_URL}/company/update/${id}`, data, {
+    const response = await axios.put(`${API_URL}/update/${id}`, data, {
         headers: getAuthHeader(),
     });
     return response.data;
@@ -34,7 +34,7 @@ export const updateCompany = async (id, data) => {
 
 // Delete a company
 export const deleteCompany = async (id) => {
-    const response = await axios.get(`${API_URL}/company/${id}/delete`, {
+    const response = await axios.get(`${API_URL}/${id}/delete`, {
         headers: getAuthHeader(),
     });
     return response.data;
